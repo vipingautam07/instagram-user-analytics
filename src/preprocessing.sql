4ace83d2a38a4bdc0f0c23d90c358b18e21a0b1c
Vipin Gautam <95081432+vipingautam07@users.noreply.github.com>
2024-09-03 23:01:07 +0530

    first commit

---
+++ src/preprocessing.sql
@@ -0,0 +1,89 @@
+/* © 2023 Tushar Aggarwal. All rights reserved. 
+@https://github.com/tushar2704/
+Instagram-User-Analytics
+*/
+--Checking and Storing all Tables--
+
+--users table
+SELECT *
+FROM users;
+
+
+--photos table
+SELECT *
+FROM photos;
+
+--comments table
+SELECT *
+FROM comments;
+
+--likes table
+SELECT *
+FROM likes;
+
+--follows table
+SELECT *
+FROM follows;
+
+--tags table
+SELECT *
+FROM tags;
+
+--photo_tags table
+SELECT *
+FROM photo_tags;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
